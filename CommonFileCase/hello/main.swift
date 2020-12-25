06dc3210572dc573b1ec5a8664874b0078efe437
邓江洲 <707703386@qq.com>
2020-12-26 01:32:58 +0800

    code opt

--- CommonFileCase/hello/main.swift
+++ CommonFileCase/hello/main.swift
@@ -9,7 +9,7 @@ import Foundation
 
 var contents: String?
 
-if let src = URL(string: "/Users/jzd/Documents/Lalathon/src/ContentView.swift"){
+if let src = URL(string: "\(NSHomeDirectory())/Documents/Lalathon/src/ContentView.swift"){
     do {
         contents = try String(contentsOfFile: src.path)
        // print(contents ?? "")
