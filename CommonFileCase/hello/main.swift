546237590c8c9101301fa4e6354609fed6630947
邓江洲 <707703386@qq.com>
2020-12-25 14:08:31 +0800

    fatalError()

--- CommonFileCase/hello/main.swift
+++ CommonFileCase/hello/main.swift
@@ -7,17 +7,22 @@
 
 import Foundation
 
-
+var contents: String?
 
 if let src = URL(string: "/Users/jzd/Documents/Lalathon/src/ContentView.swift"){
     do {
-        let contents = try String(contentsOfFile: src.path)
-        print(contents)
+        contents = try String(contentsOfFile: src.path)
+        print(contents ?? "")
     } catch {
         print(error)
     }
 }
 
 
- 
+guard let info = contents else{
+    fatalError()
+}
+
+
+
  
