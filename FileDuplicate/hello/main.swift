953ce577d54efbdad04aeba415264db1938fa872
邓江洲 <707703386@qq.com>
2021-01-11 11:22:12 +0800

    po precious

--- FileDuplicate/hello/main.swift
+++ FileDuplicate/hello/main.swift
@@ -13,9 +13,16 @@ let path = "\(NSHomeDirectory())/Downloads"
 let folder = try Folder(path: path)
 var i = 0
 let cnt = folder.files.count
+var previous: File? = nil
 for file in folder.files {
-    if file.name.contains("(1"){
-        print(file.name)
-    }
     
+    if file.name.contains("(1"){
+        if let pre = previous{
+            print(pre.name)
+        }
+        print(file.name)
+        print("----\n")
+    }
+    previous = file
 }
+
